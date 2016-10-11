ac8b80ea4bf80dd08613811fe0159487429966e5
Alexey Melezhik <melezhik@gmail.com>
2016-10-11 18:03:07 +0300

    up

--- story.bash
+++ story.bash
@@ -9,6 +9,6 @@ export q="'";
 find $local_dir -maxdepth 1 -mindepth 1 -type d -execdir perl -MFile::Basename \
 -e 'my $p = basename(@ARGV[0]); my $svn_repo = $ENV{repo_url}; 
 my $q = $ENV{q};
-print "let \"i++\"; echo -n \$i. \" \"; cd $ENV{local_dir}/$p && git init 2>/dev/null; git rm -r .; ",
-" svn export $svn_repo/$p -f . && git add .; git commit -a -m export-from-svn; echo \n"' {}  \; \
+print "let \"i++\"; echo -n \$i. \" \"; cd $ENV{local_dir}/$p && git init; git rm -r ./ --ignore-unmatch -q; ",
+" svn export $svn_repo/$p -q --force . && git add ./; git commit -a -m export-from-svn -q; echo \n"' {}  \; \
 | bash && echo done
