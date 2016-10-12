57078f032900f2d8df739a62815bad20d6a05c3e
Alexey Melezhik <melezhik@gmail.com>
2016-10-12 11:54:29 +0300

    export-from-svn

--- story.bash
+++ story.bash
@@ -5,17 +5,32 @@ export project=$(config project)
 echo svn_repo: $svn_repo
 echo local_dir: $local_dir
 
+echo
+echo
+echo
+
+test -d $local_dir || mkdir -p $local_dir
+
+echo 'updating project list from' $svn_repo ...
+
+for i in $(svn list $svn_repo); do 
+  mkdir -p $local_dir/$i
+done
+
 find $local_dir -maxdepth 1 -mindepth 1 -type d -execdir perl -MFile::Basename \
 -e '
 
 my $p = basename(@ARGV[0]); 
 my $svn_repo = $ENV{svn_repo}; 
 my $project = $ENV{project};
+my $local_dir = $ENV{local_dir};
 
 if ($project){
     exit unless $project eq $p;
 }
 
-print "let \"i++\"; echo -n \$i. \" \"; cd $ENV{local_dir}/$p && git init; git rm -r ./ --ignore-unmatch -q; ",
-" svn export $svn_repo/$p -q --force . && git add ./; git commit -a -m export-from-svn ; echo \n"' {}  \; \
-| bash && echo done
+print "let \"i++\"; echo -n \$i. \" \"; mkdir $local_dir/$p && \\
+cd $local_dir/$p && git init; git rm -r ./ --ignore-unmatch -q && \\
+svn export $svn_repo/$p -q --force . \\
+&& git add ./; git commit -a -m export-from-svn ; \\
+echo \n"' {}  \; | bash && echo done
