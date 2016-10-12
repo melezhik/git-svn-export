d7132cc2495c00bcd8117e684cd315ef101eae00
Alexey Melezhik <melezhik@gmail.com>
2016-10-12 12:16:31 +0300

    up

--- story.bash
+++ story.bash
@@ -45,9 +45,10 @@ if ($project){
 
 print "
 let \"i++\"; \\
+( \\
 echo -n \$i. \" \";  \\
 mkdir -p $local_dir/$p && \\
 cd $local_dir/$p && git init; git rm -r ./ --ignore-unmatch -q && \\
 svn export $svn_repo/$p -q --force . \\
-&& git add ./ && git commit -a -m export-from-svn ; \\
+&& git add ./ && git commit -a -m export-from-svn ; ) & \\
 echo \n"' {}  \; | bash && echo done
